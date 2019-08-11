0c596494f97f2ca246444020cd455f3b3ce6e395
Craig A Rodway <craig.rodway@gmail.com>
2019-08-11 15:49:26 +0100

    Removed legacy bookings views

--- application/views/bookings/table/headings/days.php
+++
@@ -1,3 +0,0 @@
-<td align="center" width="<?php echo $width ?>">
-	<strong><?php echo html_escape($name) ?></strong><br /><br />
-</td>
--- application/views/bookings/table/rowinfo/days.php
+++
@@ -1,5 +0,0 @@
-<td align="right" width="<?php echo $width ?>" style="padding: 15px 5px;">
-	<strong><?php echo html_escape($name) ?></strong>
-	<br />
-	<span style="font-size:90%"><?= date('jS M', strtotime($date)) ?></span>
-</td>
