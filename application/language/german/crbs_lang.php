117d57e359549cb57f2f7815dbad617355773782
Marco Lecheler <marco@task.media>
2020-05-30 19:18:50 +0200

    added language support for german

---
+++ application/language/german/crbs_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['crbs_mustbeloggedin'] = 'Du musst angemeldet sein, um auf diese Seite zuzugreifen.';
+$lang['crbs_mustbeadmin'] = 'Du musst ein Administrator sein, um auf diese Seite zuzugreifen.';
+
+$lang['crbs_action_dberror'] = 'Ein Datenbankfehler trat bei dem Elements auf (%s).';
+$lang['dberror'] = 'Ein Datenbankfehler trat auf (%s, %s).';
+$lang['crbs_action_added'] = '%s wurde erfolgreich hinzugefügt.';
+$lang['crbs_action_saved'] = '%s wurde erfolgreich gespeichert.';
+$lang['crbs_action_modified'] = '%s wurde erfolgreich bearbeitet.';
+$lang['crbs_action_deleted'] = 'Das Element wurde erfolgreich gelöscht.';
+
+$lang['colour_invalid'] = 'Du hast einen falschen Farbwert eingegeben.';
+
+?>
